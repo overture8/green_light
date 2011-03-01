74d205c1a699d739412f24561e2e2b105cd139de
Phil McClure <pmcclure@rumblelabs.com>
2011-03-01 19:03:30 +0000

    Added more tests

--- spec/green_light_spec.rb
+++ spec/green_light_spec.rb
@@ -13,6 +13,21 @@ describe GreenLight do
     rules = GreenLight::Rules.generate([PresenceOfModel])
     rules.should == "{\"errorElement\":\"span\",\"rules\":{\"presence_of_model[title]\":{\"required\":true}}}"
   end
+
+  it "should return a json string when validating the length of a field" do
+    rules = GreenLight::Rules.generate([LengthOfModel])
+    rules.should == "{\"errorElement\":\"span\",\"rules\":{\"length_of_model[title]\":{\"maxlength\":10,\"minlength\":5}}}"
+  end
+
+  it "should return a json string when validating the numericality of a field" do
+    rules = GreenLight::Rules.generate([NumericalityOfModel])
+    rules.should == "{\"errorElement\":\"span\",\"rules\":{\"numericality_of_model[age]\":{\"regex\":\"^[0-9]*$\"}}}"
+  end
+
+  it "should return a json string when validating the uniqueness of a field" do
+    rules = GreenLight::Rules.generate([UniquenessOfModel])
+    rules.should == "{\"errorElement\":\"span\",\"rules\":{\"uniqueness_of_model[title]\":{\"remote\":\"/javascripts/check_for_uniqueness?model=UniquenessOfModel&field=title\"}}}"
+  end
 end
 
 class FormatOfModel < SuperModel::Base
@@ -21,4 +36,16 @@ end
 
 class PresenceOfModel < SuperModel::Base
   validates_presence_of :title
+end
+
+class LengthOfModel < SuperModel::Base
+  validates_length_of :title, :within => 5..10
+end
+
+class NumericalityOfModel < SuperModel::Base
+  validates_numericality_of :age
+end
+
+class UniquenessOfModel < SuperModel::Base
+  validates_uniqueness_of :title
 end
