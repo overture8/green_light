5a81f08950b0e7d67c9946c3462b29a1997bba1b
Phil McClure <pmcclure@rumblelabs.com>
2011-04-04 19:34:04 +0100

    Cleaned up documentation and removed dependency on jquery version 1.4.4

--- lib/rails/generators/green_light_assets/green_light_assets_generator.rb
+++ lib/rails/generators/green_light_assets/green_light_assets_generator.rb
@@ -4,27 +4,20 @@ class GreenLightAssetsGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
   def generate_assets
-    unless File.exists?("public/javascripts/jquery.validate.min.js")
-      copy_file "jquery.validate.min.js", "public/javascripts/jquery.validate.min.js"
-    else
-      puts "public/javascripts/jquery.validate.min.js already exists."
-    end
-
     unless File.exists?("public/javascripts/jquery.validate_regex.js")
       copy_file "jquery.validate_regex.js", "public/javascripts/jquery.validate_regex.js"
     else
       puts "public/javascripts/jquery.validate_regex.js already exists."
     end
     
-    unless File.exists?("public/javascripts/jquery-1.4.4.min.js")
-      copy_file "jquery-1.4.4.min.js", "public/javascripts/jquery-1.4.4.min.js"
-    else
-      puts "public/javascripts/jquery-1.4.4.min.js already exists."
-    end
+    puts "Nearly there!  Just download the jQuery Validate plugin (http://jquery.bassistance.de/validate/jquery-validation-1.8.0.zip)"
+    puts "and jQuery 1.3.2, 1.4.2, 1.4.4, 1.5.0, 1.5.1."
     puts ""
-    puts "Insert the following code in your layout header"
+    puts "Include the above in your layout header."
     puts ""
-    puts "<%= javascript_include_tag 'jquery-1.4.4.min', 'jquery.validate.min', 'jquery.validate_regex', 'green_light' %>"
+    puts "Also, insert the following code in your layout header"
+    puts ""
+    puts "<%= javascript_include_tag 'jquery.validate_regex', 'green_light' %>"
     puts ""
   end
 end
