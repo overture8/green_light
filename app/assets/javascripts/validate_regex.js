0531ee3a26917d9fa35927dacc23d0e20e672d1c
Phil McClure <pmcclure@rumblelabs.com>
2011-08-02 15:41:37 +0100

    More Rails 3.1 asset pipeline tweeks

--- app/assets/javascripts/validate_regex.js
+++
@@ -1,9 +0,0 @@
-$.validator.addMethod(
-  "regex",
-  function(value, element, regexp) {
-    var check = false;
-    var re = new RegExp(regexp);
-    return this.optional(element) || re.test(value);
-  },
-  "You have entered an invalid value for this field"
-);
