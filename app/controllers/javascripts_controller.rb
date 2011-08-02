4dddfdee954469cdf12bdd4593ce42245b11e0b8
Phil McClure <pmcclure@rumblelabs.com>
2011-08-02 15:44:32 +0100

    More Rails 3.1 asset pipeline tweeks

--- app/controllers/javascripts_controller.rb
+++ app/controllers/javascripts_controller.rb
@@ -1,10 +1,5 @@
 class JavascriptsController < ApplicationController
-  respond_to :js, :text
-
-  def green_light
-    @rules = GreenLight::Rules.generate(all_models)
-    respond_with(@rules)
-  end
+  respond_to :text
 
   def check_for_uniqueness
     record = params[:model].constantize.where("#{params[:field]} = ?", params[params[:model].downcase.underscore][params[:field]])
@@ -14,11 +9,4 @@ class JavascriptsController < ApplicationController
       render :text => true
     end
   end
-
-  private
-    def all_models
-      tables = ActiveRecord::Base.connection.tables
-      tables.delete("schema_migrations")
-      tables.map { |table| table.camelize.singularize.constantize }
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  match "/assets/green_light" => "javascripts#green_light"
+  match "/assets/green_light" => "assets#green_light"
   match "/javascripts/check_for_uniqueness" => "javascripts#check_for_uniqueness"
 end
